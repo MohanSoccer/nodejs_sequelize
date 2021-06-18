5bc3e714add07db35908f1c98e73d4cff2f7813c
Mohan raj <mohansoccer6@gmail.com>
2021-06-18 23:35:38 +0530

    changes

--- response/array-response/array.response.js
+++
@@ -1,9 +0,0 @@
-class ArrayResponse {
-    constructor( pageSize , pageNumber , totalRecords ) {
-        this.pageSize = pageSize;
-        this.pageNumber = pageNumber;
-        this.totalRecords = totalRecords;
-    }
-}
-
-module.exports = ArrayResponse;
--- server.js
+++ server.js
@@ -1,39 +1,15 @@
 const express = require('express');
 const app = express();
-// const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerUI = require("swagger-ui-express");
-const apiErrorHandler = require('./response/api.error.handler');
-// const student = require('./src/controller/student/student.controller');
+
 
 const PORT = 3500;
 
-const swaggerOptions = {
-    swaggerDefinition: {
-        info : {
-            title :   'Library api',
-            version: '1.0.0'
-        }
-    },
-    apis: ['./src/controller/student/student.controller.js']
-}
 
-app.use(express.json())
-
-// const swaggerDocs = swaggerJsDoc(swaggerOptions);
-
-// app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs))
-
-// app.use('/student',student)
+app.use(express.json());
 
 app.get('/',(req,res) => {
     res.json("API Running");
 })
-
-app.use(apiErrorHandler);
-
-
-
-
 
 
 app.listen(PORT, (err) => {
